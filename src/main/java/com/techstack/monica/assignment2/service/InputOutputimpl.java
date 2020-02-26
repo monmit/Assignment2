0b48d751c906bfb3b486f3b266939db58e1fc2d4
monica-jftw <monica.mittal@justforthewin.com>
2020-02-26 16:37:28 +0100

    adding service classes folder

---
+++ src/main/java/com/techstack/monica/assignment2/service/InputOutputimpl.java
@@ -0,0 +1,19 @@
+package com.techstack.monica.assignment2.service;
+
+import java.util.Scanner;
+
+public class InputOutputimpl {
+    Scanner scanner = new Scanner(System.in);
+
+    public void AskInputforActions(){
+
+    }
+
+    public void AskInputForStudentDetails(){
+
+    }
+
+    public void AskInputforStudentCourses(){
+
+    }
+}
