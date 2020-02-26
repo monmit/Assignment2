52e694c8f110bc46d6a983ac76b3c30c10135653
monica-jftw <monica.mittal@justforthewin.com>
2020-02-26 16:36:40 +0100

    adding domain classes

---
+++ src/main/java/com/techstack/monica/assignment2/domain/RecordOfStudents.java
@@ -0,0 +1,8 @@
+package com.techstack.monica.assignment2.domain;
+
+public class RecordOfStudents {
+
+    public void databaseOfUsers(Student student){
+
+    }
+}
---
+++ src/main/java/com/techstack/monica/assignment2/domain/Student.java
@@ -0,0 +1,14 @@
+package com.techstack.monica.assignment2.domain;
+
+public class Student {
+    public String name;
+    public int age;
+    public String address;
+    public int rollNumber;
+
+
+
+
+
+
+}
