c9f50315c04e37b7953db0921c305e83f6e40368
monica-jftw <monica.mittal@justforthewin.com>
2020-02-27 16:17:11 +0100

    dry code

--- src/main/java/com/techstack/monica/assignment2/Main.java
+++ src/main/java/com/techstack/monica/assignment2/Main.java
@@ -1,4 +1,17 @@
 package com.techstack.monica.assignment2;
 
+import com.techstack.monica.assignment2.domain.Student;
+import com.techstack.monica.assignment2.domain.StudentList;
+import com.techstack.monica.assignment2.service.InputOutputimpl;
+
 public class Main {
+
+    public static void main(String[] args) {
+        InputOutputimpl userInput = new InputOutputimpl();
+        Student student = new Student();
+        StudentList studentList = new StudentList();
+        userInput.chooseMethod();
+
+    }
+
 }
