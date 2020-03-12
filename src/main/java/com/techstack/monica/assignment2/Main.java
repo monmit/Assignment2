0c47d0d2b7eb77e6ec072dcd3cd171d91df211ae
monica-jftw <monica.mittal@justforthewin.com>
2020-03-12 15:57:33 +0100

    Main refactored

--- src/main/java/com/techstack/monica/assignment2/Main.java
+++ src/main/java/com/techstack/monica/assignment2/Main.java
@@ -1,16 +1,19 @@
 package com.techstack.monica.assignment2;
 
-import com.techstack.monica.assignment2.domain.Student;
 import com.techstack.monica.assignment2.domain.StudentList;
-import com.techstack.monica.assignment2.service.InputOutputimpl;
+import com.techstack.monica.assignment2.service.InputOutputImpl;
 
 public class Main {
 
     public static void main(String[] args) {
-        InputOutputimpl userInput = new InputOutputimpl();
-        Student student = new Student();
-        StudentList studentList = new StudentList();
-        userInput.chooseMethod();
+        InputOutputImpl input = new InputOutputImpl();
+        input.chooseMethod();
+        StudentList actions = new StudentList();
+
+
+
+
+
 
     }
 
