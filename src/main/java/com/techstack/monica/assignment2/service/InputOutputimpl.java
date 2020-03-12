01c8dc52db5779c59e465898e9b64a590efd7743
monica-jftw <monica.mittal@justforthewin.com>
2020-03-12 12:24:50 +0100

    Implementation Changed

--- src/main/java/com/techstack/monica/assignment2/service/InputOutputimpl.java
+++ src/main/java/com/techstack/monica/assignment2/service/InputOutputimpl.java
@@ -5,36 +5,58 @@ import com.techstack.monica.assignment2.domain.StudentList;
 
 import java.util.Scanner;
 
-public class InputOutputimpl extends StudentList {
-    Scanner scanner = new Scanner(System.in);
-    int[] options = {1,2,3,4,5};
+public class InputOutputimpl {
+    //int[] options = {1,2,3,4,5};
     int choice;
+    String name;
+    int age;
+    String address;
+    int rollNumber;
+    String[] courses;
+    StudentList studentlist = new StudentList();
 
     public void chooseMethod(){
+        Scanner scanner = new Scanner(System.in);
         System.out.println("1. add, 2. display, 3. delete, 4. save, 5. exit");
         System.out.println("please choose any one option");
         choice = scanner.nextInt();
         if(choice == 1){
-
+            askStudentDetails();
+            studentlist.addStudent();
+        } else if (choice == 2){
+            studentlist.displayStudent();
+        } else if (choice == 3){
+            studentlist.deleteStudent();
+        } else if (choice == 4){
+            studentlist.saveStudent();
+        }else if (choice == 5){
+            System.out.println("Exit");
         }
-
-        }
-
-    public void askStudentDetails(Student student){
-        String name = scanner.next();
-        student.setName(name);
-        int age = scanner.nextInt();
-        student.setAge(age);
-        String address = scanner.next();
-        student.setAddress(address);
-        int rollNumber = scanner.nextInt();
-        student.setRollNumber(rollNumber);
-        String courses = scanner.next();
-        student.setCourses(courses);
 
     }
+        public void askStudentDetails(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the fullName below:");
+        name = scanner.next();
+        System.out.println("Please enter the Age below:");
+        age = scanner.nextInt();
+            // How do is include a check for non integer values?
+        System.out.println("Please enter the Address below:");
+        address = scanner.next();
+        System.out.println("Please enter the RollNumber below:");
+        rollNumber = scanner.nextInt();
+            // same here, How do is include a check for non integer values?
+        System.out.println("Please enter atleast 4 courses below");
+        askStudentCourses();
+        // how do i make this input as a student object?
+     }
 
-    public void askStudentCourses(Student student){
+    public void askStudentCourses(){
+        Scanner scanner = new Scanner(System.in);
+        while(courses.length<4){
+            String allCourses = scanner.next();
+            courses = allCourses.trim().split(",");
+        }
 
     }
 }
